af4fdede3120c663831db445527114581d824356
bharathmighty248 <bharathmighty248@gmail.com>
2021-12-06 13:37:40 +0530

    Add: Added test cases for GetAllLabels Api

---
+++ test/label.test.js
@@ -0,0 +1,37 @@
+const EasyGraphQLTester = require('easygraphql-tester');
+const fs = require('fs');
+const path = require('path');
+
+const labelSchema = fs.readFileSync(path.join(__dirname, ".", "schema.gql"), "utf8");
+
+describe("Query", () => {
+    let tester;
+    beforeAll(() => {
+        tester = new EasyGraphQLTester(labelSchema);
+    });
+
+    test("Mocking label Query", () => {
+        const query = `
+        query {
+            getAllLabels {
+                labelName
+                noteId
+            }
+        }`;
+
+        const fixture = {
+            data: {
+                getAllLabels: [{
+                    labelName: "first",
+                    noteId: [
+                        "6163d98f2137afa6e34d6c95"
+                    ]
+                }],
+            },
+        }
+        tester.setFixture(fixture);
+        const result = tester.mock({ query });
+        expect(result.data.getAllLabels[0].labelName).toBe("first");
+        expect(result.data.getAllLabels[0].noteId[0]).toBe("6163d98f2137afa6e34d6c95");
+    });
+});
