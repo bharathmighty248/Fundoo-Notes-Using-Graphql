009fcb248ed2f3b652810c25eece62cb07f2bc29
bharathmighty248 <bharathmighty248@gmail.com>
2021-12-06 13:45:35 +0530

    Add: Added test cases for GetLabels Api

--- test/label.test.js
+++ test/label.test.js
@@ -35,3 +35,36 @@ describe("Query", () => {
         expect(result.data.getAllLabels[0].noteId[0]).toBe("6163d98f2137afa6e34d6c95");
     });
 });
+
+describe("Mutations", () => {
+    let tester;
+    beforeAll(() => {
+        tester = new EasyGraphQLTester(labelSchema);
+    });
+
+    // getLabels Testcase
+    test("Mocking label mutation", () => {
+        const query = `
+        mutation {
+            getLabels {
+                labelName
+                noteId
+            }
+        }`;
+
+        const fixture = {
+            data: {
+                getLabels: [{
+                    labelName: "first",
+                    noteId: [
+                        "6163d98f2137afa6e34d6c95"
+                    ]
+                }],
+            },
+        }
+        tester.setFixture(fixture);
+        const result = tester.mock({ query });
+        expect(result.data.getLabels[0].labelName).toBe("first");
+        expect(result.data.getLabels[0].noteId[0]).toBe("6163d98f2137afa6e34d6c95");
+    });
+});
